086ef676211207d5ff1aeee7c5bfd1c038cc8ca5
rePeek <senxlin@gmail.com>
2026-09-09 19:46:14 +0800

    Align tokenizer with for statement

--- codegen.c
+++ codegen.c
@@ -115,8 +115,7 @@ static void gen_stmt(Node *node) {
   }
   case ND_FOR: {
     int c = count();
-    if (node->init)
-      gen_stmt(node->init);
+    gen_stmt(node->init);
     printf(".L.begin.%d:\n", c);
     if (node->cond) {
       gen_expr(node->cond);
